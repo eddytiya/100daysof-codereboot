182b05f0797f6afb2b364b6ecade1210d31e1e74
aditya pathak <102594671+eddytiya@users.noreply.github.com>
2024-04-27 01:03:32 +0530

    Add files via upload

---
+++ aditya.py
@@ -0,0 +1,11 @@
+def welcome():
+    print("hey u r welcome from adi")
+
+welcome()    
+
+
+#print(__name__)
+#we need to avoid the double printing if the problem
+if __name__ == "__main__":
+    welcome()
+
